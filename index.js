46bb4922d5e25f5f13cf474346547bcc987a862c
Edwin sanjo soji <72296542+edwinsanjo@users.noreply.github.com>
2022-02-04 20:11:26 +0530

    intial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+
+function isValidUrl(_string) {
+    const matchpattern = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/gm;
+    return matchpattern.test(_string);
+}
+
+app.get('/', (req, res) => {
+    if (!req.query.url) {
+        res.sendFile(path.join(__dirname + '/index.html'));
+    } else {
+
+        if (isValidUrl(req.query.url)) {
+            axios.get(req.query.url).then(resp => {
+                res.send(resp.data)
+            });
+        }
+        res.send("dont Try To Fool Me")
+    }
+});
+
+app.listen(3000, () => console.log(`Running On : http://localhost:3000`));
